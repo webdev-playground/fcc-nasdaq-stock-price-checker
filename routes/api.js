b39baeabe8cf874216babe6ec1e15b07a6b5832e
Glitch (abyssinian-taxicab-8z6bbkqn1q) <none>
2020-02-18 23:55:53 +0000

    😼🌉 Checkpoint ./routes/api.js:11249790/5570

--- routes/api.js
+++ routes/api.js
@@ -1,46 +1,59 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-const superagent = require('superagent');
+var expect = require("chai").expect;
+const superagent = require("superagent");
 
-module.exports = function (app) {
+module.exports = function(app) {
+  app.route("/api/stock-prices").get(async function(req, res) {
+    let { stock, like } = req.query;
+    //       if (Array.isArray(stock)) {
 
-  app.route('/api/stock-prices')
-    .get(async function (req, res){
-      const { stock, like } = req.query;
-//       if (Array.isArray(stock)) {
-        
-//       }
-      try {
-        const data = await superagent.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`);
-
-        if (data.body === 'Unknown symbol') {
-          throw new Error('Unknown symbol');
-        }
-        
-        console.log(data.body.latestPrice);
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
+    //       }
+    try {
+      if (!Array.isArray(stock)) {
+        stock = [stock];
       }
-    });
+      
+      const data = []
+
+      for (let st of stock) {
+        const price = await getStockPrice(st);
+        const likes = 0;
+        
+        const stData = { stock: st, price: price, likes: likes };
+        
+        data.push(stData);
+      }
+
+      return res
+        .status(200)
+        .json({ stockData: data });
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  });
 };
 
 async function getStockPrice(stock) {
-      try {
-        const data = await superagent.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`);
+  try {
+    const data = await superagent.get(
+      `https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`
+    );
 
-        if (data.body === 'Unknown symbol') {
-          throw new Error('Unknown symbol');
-        }
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
-      } 
+    if (data.body === "Unknown symbol") {
+      throw new Error("Unknown symbol");
+    }
+
+    return data.body.latestPrice;
+  } catch (err) {
+    throw err;
+  }
 }
