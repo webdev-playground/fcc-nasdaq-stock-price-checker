353c1e7ed93aeab4d040fabd345ca3401a4062ec
Glitch (abyssinian-taxicab-8z6bbkqn1q) <none>
2020-02-19 00:30:18 +0000

    ⛄️🎓 Checkpoint ./routes/api.js:11249790/4809

--- routes/api.js
+++ routes/api.js
@@ -10,49 +10,69 @@
 
 var expect = require("chai").expect;
 const superagent = require("superagent");
+const Stock = require("../models/stock");
+const IP = require("../models/ip");
 
 module.exports = function(app) {
   app.route("/api/stock-prices").get(async function(req, res) {
     let { stock, like } = req.query;
-    
+
     try {
       if (!stock) {
-        throw new Error('stock symbol needs to be provided in query');
+        throw new Error("stock symbol needs to be provided in query");
       }
-      
+
       if (!Array.isArray(stock)) {
         stock = [stock];
       }
-      
+
       if (stock.length > 2) {
-        throw new Error('a maximum of 2 stocks can be specified');
+        throw new Error("a maximum of 2 stocks can be specified");
       }
-      
-      const data = []
+
+      const data = [];
 
       for (let st of stock) {
         const price = await getStockPrice(st);
         const likes = like ? 1 : 0;
-        
-        // CHeck if IP address 
+
+        let stockLikes;
+        // Check if IP address has already been recorded
         if (likes > 0) {
           const ip = req.ip;
-          if 
+          const foundIp = await IP.findOne({ ip: ip });
+          if (!foundIp) {
+            // store IP in db
+            await IP.save({ ip: ip });
+
+            // increment likes
+            stockLikes = await Stock.findOneAndUpdate(
+              { stock: st },
+              {
+                $inc: {
+                  likes: 1
+                }
+              },
+              { new: true }
+            );
+          }
         }
         
-        const stData = { stock: st, price: price, likes: likes };
-        
+        if (!stockLikes) {
+          stockLikes = await Stock.findOne({ stock: st });
+        }
+
+        const stData = { stock: st, price: price, likes: stockLikes.likes };
+
         data.push(stData);
       }
-      
+
       const stockData = { stockData: data };
       if (data.length === 1) {
         stockData.stockData = data[0];
       }
 
-      return res
-        .status(200)
-        .json(stockData);
+      return res.status(200).json(stockData);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
